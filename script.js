d72f8700311b77566e0f0f530059baa1515a1b42
victoronyekachi-hub <victoronyekachi070@gmail.com>
2026-09-10 13:06:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ function multiply() {
     let number2 = Number(document.getElementById("number2").value);
 
     let result = number1 * number2;
+function divide() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
 
+    if (number2 === 0) {
+        document.getElementById("result").textContent = "Cannot divide by zero";
+        return;
+    }
+
+    let result = number1 / number2;
+
+    document.getElementById("result").textContent = result;
+}
     document.getElementById("result").textContent = result;
 }
