ae4401d943b25164ac7d8fb0537f7c84d4b3bce7
victoronyekachi-hub <victoronyekachi070@gmail.com>
2026-09-10 12:46:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function add() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+
+    let result = number1 + number2;
+
+    document.getElementById("result").textContent = result;
+}
