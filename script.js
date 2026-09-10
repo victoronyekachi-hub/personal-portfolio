e0b9ca79893172f5a6409cdda84235d98ccb3771
victoronyekachi-hub <victoronyekachi070@gmail.com>
2026-09-10 12:59:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function subtract() {
 
     document.getElementById("result").textContent = result;
 }
+function multiply() {
+    let number1 = Number(document.getElementById("number1").value);
+    let number2 = Number(document.getElementById("number2").value);
+
+    let result = number1 * number2;
+
+    document.getElementById("result").textContent = result;
+}
