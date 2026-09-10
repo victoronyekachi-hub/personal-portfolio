5455d2f089477360f6977a981d8ff9464116f05b
victoronyekachi-hub <victoronyekachi070@gmail.com>
2026-09-10 14:50:18 +0100

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,4 +1,10 @@
-console.log("Victor");
-console.log(18);
-console.log("FUASK");
-console.log("bsc computer science");
+
+let name = "Victor";
+let age = "18";
+let university = "FUASK";
+let course = "Bsc Computer Science";
+
+console.log(name);
+console.log(age);
+console.log(university);
+console.log(course);
